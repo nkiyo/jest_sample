895730ab1e5203d642a7977b271460d0e9cd51da
nkiyo <nkiyo@hoge.com>
2019-02-21 10:37:06 +0900

    succeed.

--- sum.js
+++ sum.js
@@ -2,6 +2,4 @@ function sum(a, b) {
     return a + b;
 }
 
-//console.log(`sum is ${sum(1, 2)}.`);
-
-export default sum;
+module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,6 @@
-import sum from './sum';
+const sum = require('./sum');
 
 test('test sum', () => {
-    expect(sum(1, 2).toBe(3));
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(2, 3)).toBe(5);
 });
